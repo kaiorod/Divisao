7b6e6672ef1d7ce6f0c7a7bee18c749ea3e9b465
kaiorod <caiorodrigo2775@gmail.com>
2021-10-08 20:28:51 -0300

    Finaliza Divisao

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num, den, div;
+
+            Console.Write("Digite o numerador : ");
+            num = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o denominador : ");
+            den = Convert.ToDouble(Console.ReadLine());
+
+            div = num / den;
+
+            if(den == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Não é possível dividir por zero");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine($"{num} dividido por {den} é {div}");
+            }
+
+
+        }
+    }
+}
